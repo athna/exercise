2c713e0239382e3791c76e1a9b18356d7f079fa8
Yoshiki Makino <ymakino@users.noreply.github.com>
2019-11-14 03:37:26 +0900

    Add files via upload

---
+++ humming/heater_off.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NUM=21
+
+if [ \! -d /sys/class/gpio/gpio${NUM} ]; then
+  echo ${NUM} > /sys/class/gpio/export
+  sleep 0.5
+  echo out > /sys/class/gpio/gpio${NUM}/direction
+fi
+
+echo 0 > /sys/class/gpio/gpio${NUM}/value
